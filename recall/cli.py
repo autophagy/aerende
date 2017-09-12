ac9ae7e955d89b0ee06ba24f84e4f61a8fd0b9eb
Autophagy <mika@crate.io>
2017-09-12 21:14:02 +0200

    Changed option to specify config, along with default

--- recall/cli.py
+++ recall/cli.py
@@ -1,21 +1,28 @@
-import os
+from os import path, makedirs
 import argparse
+import yaml
+from shutil import copy
 
 def parse_args():
     parser = argparse.ArgumentParser(description='recall')
-    parser.add_argument('-d', '--data', default='~/.recall/data.yml',
-                        help='The recall data yaml')
+    parser.add_argument('-c', '--config', default='~/.recall/config.yml',
+                        help='The recall config file')
     return parser.parse_args()
 
 
-def validate_data_path(file):
-    expanded_path = os.path.expanduser(file)
-    os.makedirs(os.path.dirname(expanded_path), exist_ok=True)
+def get_config(file):
+    expanded_path = path.expanduser(file)
+    if not path.exists(expanded_path):
+        makedirs(path.dirname(expanded_path))
+        copy(path.join(path.dirname(__file__), 'config/default.yml'), expanded_path)
+
+    with open(expanded_path, 'r') as config_file:
+        return yaml.load(config_file)
 
 
 def main():
     args = parse_args()
-    validate_data_path(args.data)
+    config = get_config(args.config)
 
 
 if __name__ == '__main__':
