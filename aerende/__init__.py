5e9c59faef8e2b290eed2653ee9a87ca50052748
Autophagy <mika@crate.io>
2017-12-05 12:17:05 +0100

    Prepare 0.2.0

--- aerende/__init__.py
+++ aerende/__init__.py
@@ -3,7 +3,7 @@
 __title__ = "aerende"
 __author__ = "Mika Naylor (Autophagy)"
 __license__ = "MIT"
-__version__ = (0, 1, 1)
+__version__ = (0, 2, 0)
 
 version = "%s.%s.%s" % __version__
 title = __title__
--- doc/release_notes/index.rst
+++ doc/release_notes/index.rst
@@ -7,6 +7,14 @@ Information about each Ærende release.
 0.X
 ===
 
+0.2.X
+-----
+
+.. toctree::
+    :maxdepth: 1
+
+    0.2.0
+
 0.1.X
 -----
 
